e55fbca3becd3e250a80bf2ec62343eba6eca27d
Nati Forish <forish.nati@gmail.com>
2024-03-27 01:36:38 +0200

    dynamic page is now functioning. also 'Island check' [e.g: Cyprus]

--- details.js
+++ details.js
@@ -11,9 +11,33 @@ document.addEventListener('DOMContentLoaded', () => {
         return values.join(", ");
     }
 
-    getBorderCountries = (borderCountries) => {
-        const values = Object.values(borderCountries);
-        return values.join(", ");
+    getBorderCountries = async (borderCodes) => {
+        const borderNames = [];
+
+        console.log(borderCodes.length)
+        if (borderCodes.length === 0) {
+            return 'No border countries - This is an island!';
+        }
+
+        for (const code of borderCodes) {
+            try {
+                const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch data for ${code}`);
+                }
+                const data = await response.json();
+                console.log(data); // Log the entire data object
+                borderNames.push(data[0].name.common); // This line may cause the TypeError
+            } catch (error) {
+                console.error(error);
+                borderNames.push(`Error fetching data for ${code}`);
+            }
+        }
+
+        if (borderNames.length === 0) {
+            return 'No border countries - This is an island!';
+        }
+        return borderNames.join(", ");
     }
 
     // Fetch country data based on the name
@@ -24,24 +48,21 @@ document.addEventListener('DOMContentLoaded', () => {
             }
             return response.json();
         })
-        .then(data => {
+        .then(async data => {
             const country = data[0]; // Assuming the API returns data in an array
 
             const nativeNames = country.name.nativeName;
             const firstLanguage = Object.values(nativeNames)[0];
 
-            const currecy = country.currencies;
-            const firstCurrency = Object.values(currecy)[0];
-
-            // Populate country details on the page
-            countryDetailsContainer.innerHTML = `
-                <img src="${country.flags.svg}" alt="${country.name.common} Flag" style="width: 50px; height: 30px; />
-                <h1>${country.name.common}</h1>
-                <div>Population: ${country.population}</div>
-                <div>Region: ${country.region}</div>
-                <div>Capital: ${country.capital}</div>
-                <!-- Add more details as needed -->
-            `;
+            const currency = country.currencies;
+            const firstCurrency = Object.values(currency)[0];
+            
+            let borderCountries = '';
+            if (country.borders && country.borders.length > 0) {
+                borderCountries = await getBorderCountries(country.borders);
+            } else {
+                borderCountries = 'No border countries - This is an island!';
+            }
 
             countryDetailsContainer.innerHTML = `
                 <div class="parent">
@@ -62,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div class="div-span-info"><span class="span-info">Languages: </span>${getLanguages(country.languages)}</div>
                 </div>
                 <div class="div5">
-                    <div class="border-div"><span class="span-info">Border countries: </span></div>
+                    <div class="border-div"><span class="span-info">Border countries: </span>${borderCountries}</div>
                 </div>
                 </div>
             `;
